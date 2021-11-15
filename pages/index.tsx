79b34588ba09da2583791acf6c757cfa0d9f8f32
takumi-13 <takumi.gameover13@gmail.com>
2021-11-15 13:10:49 +0900

    create square

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,6 +112,21 @@ const Footer = styled.footer`
     justify-content: center;
   }
 `
+const Board = styled.canvas`
+  width: 450px;
+  height: 450px;
+  background-color: grey;
+`
+/*
+function drawBoard(): void {
+  const canvas = document.createElement('canvas');
+  canvas.setAttribute('width', '450px')
+  canvas.setAttribute('height', '450px')
+  const ctx = canvas.getContext('2d')
+  ctx.beginPath()
+  ctx.rect(25, 25, 475, 475)
+}
+*/
 
 const Logo = styled.span`
   height: 1em;
@@ -128,35 +143,9 @@ const Home: NextPage = () => {
       </Head>
 
       <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
+        <Title>Welcome to Minesweeper!!</Title>
 
-        <Description>
-          Get started by editing <Code>pages/index.js</Code>
-        </Description>
-
-        <Grid>
-          <Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
-
-          <Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-
-          <Card href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </Card>
-
-          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </Card>
-        </Grid>
+        <Board></Board>
       </Main>
 
       <Footer>
