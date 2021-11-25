c8dbc588ee3e354ebc397badd42966f9f885aef2
solufa <solufa2020@gmail.com>
2021-11-25 17:22:43 +0900

    feat: settings for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+    : '',
 }
