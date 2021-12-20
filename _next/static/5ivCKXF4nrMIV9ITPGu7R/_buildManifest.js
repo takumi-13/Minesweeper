703669b47fd141c427e78ccbd44e71859f5e9171
hideto2112 <hideto2112@users.noreply.github.com>
2021-12-20 05:57:11 +0000

    deploy: 51c5384bc050cc00d564f68ea14f569d7d2c1121

---
+++ _next/static/5ivCKXF4nrMIV9ITPGu7R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/65-2a20bed5b5213efd8e13.js","static/chunks/pages/index-3b92896acf1d3684d2d1.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
